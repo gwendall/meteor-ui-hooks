d747b1a001b70e6dd41c31baaa42a0a85f9a3d85
Max Nowack <max.nowack@oafmedium.de>
2015-05-22 14:06:29 +0200

    prevent overriding existing hooks

--- lib.js
+++ lib.js
@@ -9,16 +9,25 @@ Template.prototype.uihooks = function(hooksAll) {
       var itemsContainer = tpl.$(selector).length && tpl.$(selector).first().parent();
       var container = declaredContainer || itemsContainer || tplContainer;
       $(container).each(function() {
+        var current = this._uihooks;
+        console.log(current);
+
         this._uihooks = {
           insertElement: function(node, next) {
+            current && current.insertElement && current.insertElement.apply(this, [node, next]);
+
             if (!$(node).is(selector)) return;
             hooks.insert && hooks.insert.apply(this, [node, next, tpl]);
           },
           moveElement: function(node, next) {
+            current && current.moveElement && current.moveElement.apply(this, [node, next]);
+
             if (!$(node).is(selector)) return;
             hooks.move && hooks.move.apply(this, [node, next, tpl]);
           },
           removeElement: function(node) {
+            current && current.removeElement && current.removeElement.apply(this, [node]);
+
             if (!$(node).is(selector)) return;
             hooks.remove && hooks.remove.apply(this, [node, tpl]);
           }
