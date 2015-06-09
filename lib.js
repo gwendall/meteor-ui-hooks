4e95167841ea197eb676c493877ebbfdd994d0fc
Max Nowack <max@unsou.de>
2015-06-09 10:33:24 +0200

    fixed applying default hooks

--- lib.js
+++ lib.js
@@ -1,3 +1,25 @@
+var allFalse = function(array) {
+  for (var elm in array) {
+    if (elm) return false
+  }
+  return true;
+};
+
+var makeHook = function(hooks, defaultCallback) {
+  return function() {
+    var args = arguments;
+    var outcome = [];
+    var self = this;
+    hooks.forEach(function(hook){
+      outcome.push(hook.apply(self, args));
+    });
+    if (!defaultCallback) return;
+    if (allFalse(outcome)) {
+      return defaultCallback.apply(self, args);
+    }
+  }
+};
+
 Template.prototype.uihooks = function(hooksAll) {
   var tpl = this;
   tpl.onRendered(function() {
@@ -9,27 +31,39 @@ Template.prototype.uihooks = function(hooksAll) {
       var itemsContainer = tpl.$(selector).length && tpl.$(selector).first().parent();
       var container = declaredContainer || itemsContainer || tplContainer;
       $(container).each(function() {
-        var current = this._uihooks;
+        if (!this._alluihooks) {
+          this._alluihooks = {
+            insert: [],
+            move: [],
+            remove: []
+          };
+        }
+        this._alluihooks.insert.push(function(node, next) {
+          if (!$(node).is(selector)) return false;
+          hooks.insert && hooks.insert.apply(this, [node, next, tpl]);
+          return true;
+        });
+
+        this._alluihooks.move.push(function(node, next) {
+          if (!$(node).is(selector)) return false;
+          hooks.move && hooks.move.apply(this, [node, next, tpl]);
+          return true;
+        });
+
+        this._alluihooks.remove.push(function(node) {
+          if (!$(node).is(selector)) return false;
+          hooks.remove && hooks.remove.apply(this, [node, tpl]);
+          return true;
+        });
 
         this._uihooks = {
-          insertElement: function(node, next) {
-            current && current.insertElement && current.insertElement.apply(this, [node, next]);
-
-            if (!$(node).is(selector)) return;
-            hooks.insert && hooks.insert.apply(this, [node, next, tpl]);
-          },
-          moveElement: function(node, next) {
-            current && current.moveElement && current.moveElement.apply(this, [node, next]);
-
-            if (!$(node).is(selector)) return;
-            hooks.move && hooks.move.apply(this, [node, next, tpl]);
-          },
-          removeElement: function(node) {
-            current && current.removeElement && current.removeElement.apply(this, [node]);
-
-            if (!$(node).is(selector)) return;
-            hooks.remove && hooks.remove.apply(this, [node, tpl]);
-          }
+          insertElement: makeHook(this._alluihooks.insert, function(node, next) {
+            return $(node).insertBefore(next);
+          }),
+          moveElement: makeHook(this._alluihooks.move),
+          removeElement: makeHook(this._alluihooks.remove, function(node) {
+            return $(node).remove();
+          })
         };
       });
     });
